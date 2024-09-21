d6fb916c30c649cd4f04e5c57692b51a12f5224b
suci06 <suciarrasyid47@gmail.com>
2024-09-21 15:17:49 +0700

    comit ke-2

---
+++ main.py
@@ -0,0 +1,22 @@
+# nilai = int(input("masukkan nilai: "))
+
+# if (nilai > 80):
+#     print("selamat anda lulus, dengan nilai yang memuaskan")
+# elif (nilai > 90 and nilai <= 100):
+#     print("selamat anda lulus")
+# else:
+#     print("anda masih belum lulus tes, semangat dan coba lagi")
+
+# for loop
+# for i in range(10, 0, -2):
+    # print(i)
+    # print("hello world")
+
+# angka = 30
+
+# while(angka > 1):
+#     print(angka)
+#     angka -= 1 # angka = angka - 1
+
+# [FUNCTION]
+# def
